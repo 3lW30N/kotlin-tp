bf6dd37b743739e62f0ea7fe56ba6fd71fee163b
3lW30N <noemierialland2004@gmail.com>
2026-04-03 16:16:38 +0200

    feat: fin du tp

--- app/src/main/java/fr/epf/sni/gc/AddClientActivity.kt
+++ app/src/main/java/fr/epf/sni/gc/AddClientActivity.kt
@@ -5,6 +5,10 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "AddClientActivity"
@@ -16,8 +20,38 @@ class AddClientActivity : AppCompatActivity() {
         val lastnameEditText = findViewById<EditText>(R.id.add_client_lastname_edittext)
         val addButton = findViewById<Button>(R.id.add_client_button)
 
+        val genderGroup = findViewById<RadioGroup>(R.id.add_client_form_gender_radiobutton)
+
+
         addButton.setOnClickListener {
+            val checkedRadioButtonId = genderGroup.checkedRadioButtonId
+            val gender = if ( checkedRadioButtonId == R.id.add_client_form_woman_radiobutton) {
+                "Femme"
+            }
+            else {
+                "Homme"
+            }
+            Log.d(TAG, "Genre: ${gender}")
+            Toast.makeText(this, "Super !", Toast.LENGTH_SHORT).show()
             Log.d(TAG, "Nom: ${lastnameEditText.text}")
         }
+
+        val ageTextView = findViewById<TextView>(R.id.add_client_form_age_textview)
+        val ageSeekBar = findViewById<SeekBar>(R.id.add_client_form_age_seekbar)
+
+        ageSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                p0: SeekBar?,
+                progress: Int,
+                p2: Boolean
+            ) {
+                ageTextView.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) = Unit;
+
+            override fun onStopTrackingTouch(p0: SeekBar?) = Unit;
+
+        })
     }
 }
